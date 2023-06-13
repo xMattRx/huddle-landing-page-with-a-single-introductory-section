811cc8c652c7f18dc6edb63fa7df745c6b7b1e76
Matthew Nascimento <matthewromie@hotmail.com>
2023-06-12 22:06:47 -0300

    desktop model ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 import styles from './App.module.scss';
-import image_desktop from './assets/bg-desktop.svg';
-import image_mobile from './assets/bg-mobile.svg';
+import main_image from './assets/illustration-mockups.svg';
 import logo from './assets/logo.svg';
 
 function App() {
@@ -11,12 +11,28 @@ function App() {
       className={styles.container}
     >
       <div className={styles.background}>
-      <header>
-        <img src={logo} alt="" />
-      </header>
-      <main>
-        
-      </main>
+        <header className={styles.header}>
+          <img src={logo} alt="logo" className={styles.logo} />
+        </header>
+        <main className={styles.main}>
+          <img src={main_image} />
+          <section className={styles.section}>
+            <h2 className={styles.title}>Build The Community Your Fans Will Love</h2>
+            <p className={styles.paragraph}>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+            <button className={styles.button}>Register</button>
+          </section>
+        </main>
+        <footer className={styles.footer}>
+          <div className={styles.icon}>
+            <FaFacebookF size="18px" />
+          </div>
+          <div className={styles.icon}>
+            <FaTwitter size="18px" />
+          </div>
+          <div className={styles.icon}>
+            <FaInstagram size="18px" />
+          </div>
+        </footer>
       </div>
     </div>
   )
